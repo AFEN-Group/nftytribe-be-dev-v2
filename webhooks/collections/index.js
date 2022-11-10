96104e1648110ada77eab81b6a57fd03a5c18a20
radiancegeorge <radiancegeorge@outlook.com>
2022-11-10 16:58:23 +0100

    updates

--- webhooks/collections/index.js
+++ webhooks/collections/index.js
@@ -4,23 +4,27 @@ const collections = require("express").Router();
 
 collections.route("/").post(
   expressAsyncHandler(async (req, res) => {
-    if (req.body.confirmed) {
-      const data = {
-        timestamp: req.body.block.timestamp,
-        chainId: req.body.chainId,
-        contractAddress: "0x" + String(req.body.logs[0].topic1).slice(26),
-        walletAddress: "0x" + String(req.body.logs[0].topic2).slice(26),
-      };
+    try {
+      if (req.body.confirmed) {
+        const data = {
+          timestamp: req.body.block.timestamp,
+          chainId: req.body.chainId,
+          contractAddress: "0x" + String(req.body.logs[0].topic1).slice(26),
+          walletAddress: "0x" + String(req.body.logs[0].topic2).slice(26),
+        };
 
-      console.log(data);
+        console.log(data);
 
-      new Collections().importCollection(
-        data.contractAddress,
-        data.chainId,
-        data.walletAddress
-      );
-    } else {
-      console.log("not confirmed");
+        new Collections().importCollection(
+          data.contractAddress,
+          data.chainId,
+          data.walletAddress
+        );
+      } else {
+        console.log("not confirmed");
+      }
+    } catch (err) {
+      console.log(err);
     }
     res.send();
   })
