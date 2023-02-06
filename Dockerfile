2c05686e2547ae8bb782a1e22855ba28493f3e5e
radiancegeorge <radiancegeorge@outlook.com>
2023-02-06 07:03:00 +0100

    closed all ports and deleted all env in deployment

--- Dockerfile
+++ Dockerfile
@@ -5,4 +5,4 @@ WORKDIR /app
 COPY . .
 RUN yarn
 EXPOSE 4000
-CMD ["yarn", "staging"]
+CMD ["yarn", "start:stging"]
--- scripts/db/set_env.js
+++ scripts/db/set_env.js
@@ -1,11 +1,39 @@
 const mysql = require("mysql2");
+const fs = require("fs");
 
 const createENVFile = async () => {
-  mysql.createConnection({
-    user: process.env.db_username || "root",
-    password: process.env.db_password,
-    host: process.env.db_host || "localhost",
-    database: process.env.database || "nftytribe",
-    port: process.env.db_port || 3306,
+  await new Promise((resolve, reject) => {
+    try {
+      const sql = mysql.createConnection({
+        user: process.env.db_username || "root",
+        password: process.env.db_password,
+        host: process.env.db_host || "localhost",
+        database: process.env.database || "nftytribe",
+        port: process.env.db_port || 3306,
+      });
+      sql.connect((err) => {
+        if (err) process.exit(1);
+        const query = `select * from configs`;
+        sql.query(query, (err, result, fields) => {
+          if (err) process.exit(1);
+          fs.open("./.env", "w", (err, fd) => {
+            if (err) exit;
+            result.forEach(({ name, value }) => {
+              fs.write(
+                fd,
+                `${name}=${value}\n`,
+                (err) => err && process.exit(1)
+              );
+            });
+            sql.end();
+            resolve();
+          });
+        });
+      });
+    } catch (err) {
+      process.exit(1);
+    }
   });
 };
+
+createENVFile();
