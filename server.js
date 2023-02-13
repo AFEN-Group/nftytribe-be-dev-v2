bcde9a5e19f8e1ce9693e807b3eb173861b3bf59
radiancegeorge <radiancegeorge@outlook.com>
2023-02-13 06:30:36 +0100

    adding gracefully shutting down

--- server.js
+++ server.js
@@ -2,6 +2,20 @@ const server = require("./app");
 const db = require("@models");
 const port = process.env.app_port;
 
+//gracefully shutting down
+process.on("SIGTERM", () => {
+  console.log("Received SIGTERM, shutting down gracefully");
+  server.close(() => {
+    console.log("Closed out remaining connections");
+    process.exit(0);
+  });
+
+  setTimeout(() => {
+    console.error("Could not close connections in time, forcing shut down");
+    process.exit(1);
+  }, 10000);
+});
+
 db.sequelize.sync().then(() => {
   server.listen(port, console.log.bind(this, `listening on port ::: ${port}`));
 });
