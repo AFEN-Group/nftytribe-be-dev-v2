cb1a054f2cf590a552ad3671a9162171087ceb69
radiancegeorge <radiancegeorge@outlook.com>
2023-04-19 12:57:52 +0100

    updates

--- config/config.js
+++ config/config.js
@@ -1,7 +1,7 @@
 module.exports = {
   username: process.env.db_username,
   password: process.env.db_password,
-  database: process.env.db_name,
+  database: process.env.db_name || process.env.database,
   host: process.env.db_host,
   dialect: "mysql",
   port: process.env.db_port,
