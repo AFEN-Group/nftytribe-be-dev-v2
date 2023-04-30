2ff23d06158845f6cd3f535783d4bcf6e81a68f5
radiancegeorge <radiancegeorge@outlook.com>
2023-04-30 21:05:04 +0100

    updates

--- webhooks/broker/index.js
+++ webhooks/broker/index.js
@@ -103,151 +103,155 @@ const { logger } = require("@helpers/logger");
 
 broker.route("/physical-item").post(async (req, res) => {
   const { txs, chainId, confirmed, logs } = req.body;
-  const { input, fromAddress, toAddress } = txs[0];
-  if (!confirmed && txs.length) {
-    console.log("passed--- confirmed");
-    const nfts = new Nfts();
-    const { name, params } = abiDecoder.decodeMethod(input);
-    if (name.toLowerCase() === "buy") {
-      console.log("passed--- buy");
-      const nft = new Nfts();
-      //get arrange fields
-      const { tokenId, erc721, price } = nft.getFields(params);
-      //get listing
-      const listing = await db.nfts.findOne({
-        where: {
-          tokenId,
-          moreInfo: {
-            contractAddress: erc721,
+  if (txs[0]) {
+    const { input, fromAddress, toAddress } = txs[0];
+    if (!confirmed && txs.length) {
+      console.log("passed--- confirmed");
+      const nfts = new Nfts();
+      const { name, params } = abiDecoder.decodeMethod(input);
+      if (name.toLowerCase() === "buy") {
+        console.log("passed--- buy");
+        const nft = new Nfts();
+        //get arrange fields
+        const { tokenId, erc721, price } = nft.getFields(params);
+        //get listing
+        const listing = await db.nfts.findOne({
+          where: {
+            tokenId,
+            moreInfo: {
+              contractAddress: erc721,
+            },
           },
-        },
-        include: [
-          {
-            model: db.physicalItems,
-            required: true,
-          },
-          {
-            model: db.users,
-            include: [
-              {
-                model: db.addresses,
-                required: true,
-              },
-            ],
-          },
-        ],
-      });
+          include: [
+            {
+              model: db.physicalItems,
+              required: true,
+            },
+            {
+              model: db.users,
+              include: [
+                {
+                  model: db.addresses,
+                  required: true,
+                },
+              ],
+            },
+          ],
+        });
 
-      if (!listing) {
-        //register an error, an attempt to purchase something that does not exist
-        return;
-      }
-      console.log("passed--- listing");
-      const listingPrice = Number(listing.price);
+        if (!listing) {
+          //register an error, an attempt to purchase something that does not exist
+          return;
+        }
+        console.log("passed--- listing");
+        const listingPrice = Number(listing.price);
 
-      const paidPrice =
-        Number(price) / 10 ** Number(listing.moreInfo.erc20TokenDecimals);
+        const paidPrice =
+          Number(price) / 10 ** Number(listing.moreInfo.erc20TokenDecimals);
 
-      const cachedData = JSON.parse(
-        await redis.get(`${listing.id}-${fromAddress.toLowerCase()}-booking`)
-      );
-
-      // console.log(fromAddress, listing.id);
-      if (!cachedData) {
-        //handle or log error and refund
-        return;
-      }
-      console.log("passed--- cached data");
-      const shippingCostUSD = cachedData.totalUsd;
-
-      const erc20Token =
-        env === "production"
-          ? listing.dataValues.moreInfo.erc20TokenAddress
-          : "0xbA2aE424d960c26247Dd6c32edC70B295c744C43";
-
-      const priceData = await Moralis.EvmApi.token
-        .getTokenPrice({
-          address: erc20Token,
-          chain: env === "production" ? chainId : EvmChain.BSC,
-        })
-        .catch(console.error);
-
-      const { usdPrice } = priceData.toJSON();
-      const shippingCostUSDToChargedToken = shippingCostUSD / usdPrice;
-
-      //adding nft price to generated equivalent
-      const totalCharge = shippingCostUSDToChargedToken + listingPrice;
-
-      const privateKey = process.env.p_key;
-      const web3 = await initWeb3(chainId);
-      const account = web3.eth.accounts.privateKeyToAccount(privateKey);
-      web3.eth.accounts.wallet.add(account);
-      const contract = new web3.eth.Contract(PIProxyAbi, toAddress);
-      let data;
-      if (paidPrice >= totalCharge) {
-        //positive -- process release of nft and shipping
-        const booked = await BubbleDelivery.book(cachedData.data).catch(
-          (err) => {
-            // log error and refund user possibly
-            logger(JSON.stringify(err), "piProxy-listing", "error");
-          }
+        const cachedData = JSON.parse(
+          await redis.get(`${listing.id}-${fromAddress.toLowerCase()}-booking`)
         );
-        await db.shipments.create({
-          order: booked.data,
-          senderId: listing.userId,
-          receiverId: (
-            await db.users.findOne({
-              where: {
-                walletAddress: fromAddress,
-              },
-            })
-          ).id,
-        });
 
-        const newSales = await nfts.buyNft(params, fromAddress);
-        data = contract.methods.processBid(logs[0].topic1, false).encodeABI();
+        // console.log(fromAddress, listing.id);
+        if (!cachedData) {
+          //handle or log error and refund
+          return;
+        }
+        console.log("passed--- cached data");
+        const shippingCostUSD = cachedData.totalUsd;
 
-        // ..notifcations
-        const worker = new Worker("./workers/singleNotifications.js");
-        const notificationData = {
-          transactionId: newSales.id,
-          title: newSales.listingInfo.name,
-          tokenId: newSales.listingInfo.tokenId,
-          type: NotificationTypes.SOLD,
-          userId: newSales.sellerId,
-          // buyerId: newSales.buyerId,
+        const erc20Token =
+          env === "production"
+            ? listing.dataValues.moreInfo.erc20TokenAddress
+            : "0xbA2aE424d960c26247Dd6c32edC70B295c744C43";
+
+        const priceData = await Moralis.EvmApi.token
+          .getTokenPrice({
+            address: erc20Token,
+            chain: env === "production" ? chainId : EvmChain.BSC,
+          })
+          .catch(console.error);
+
+        const { usdPrice } = priceData.toJSON();
+        const shippingCostUSDToChargedToken = shippingCostUSD / usdPrice;
+
+        //adding nft price to generated equivalent
+        const totalCharge = shippingCostUSDToChargedToken + listingPrice;
+
+        const privateKey = process.env.p_key;
+        const web3 = await initWeb3(chainId);
+        const account = web3.eth.accounts.privateKeyToAccount(privateKey);
+        web3.eth.accounts.wallet.add(account);
+        const contract = new web3.eth.Contract(PIProxyAbi, toAddress);
+        let data;
+        if (paidPrice >= totalCharge) {
+          //positive -- process release of nft and shipping
+          const booked = await BubbleDelivery.book(cachedData.data).catch(
+            (err) => {
+              // log error and refund user possibly
+              logger(JSON.stringify(err), "piProxy-listing", "error");
+            }
+          );
+          await db.shipments.create({
+            order: booked.data,
+            senderId: listing.userId,
+            receiverId: (
+              await db.users.findOne({
+                where: {
+                  walletAddress: fromAddress,
+                },
+              })
+            ).id,
+          });
+
+          const newSales = await nfts.buyNft(params, fromAddress);
+          data = contract.methods.processBid(logs[0].topic1, false).encodeABI();
+
+          // ..notifcations
+          const worker = new Worker("./workers/singleNotifications.js");
+          const notificationData = {
+            transactionId: newSales.id,
+            title: newSales.listingInfo.name,
+            tokenId: newSales.listingInfo.tokenId,
+            type: NotificationTypes.SOLD,
+            userId: newSales.sellerId,
+            // buyerId: newSales.buyerId,
+          };
+          worker.postMessage(notificationData);
+          console.log("Booked! ------");
+        } else {
+          //underpayment --refund user and possibly email user of failed purchase attempt
+          data = contract.methods.processBid(logs[0].topic1, true).encodeABI();
+          // console.log("underpriced");
+        }
+
+        //finish tx
+        const tx = {
+          from: account.address,
+          to: toAddress,
+          data,
+          gas: 100000,
+          gasPrice: web3.utils.toWei("50", "gwei"),
         };
-        worker.postMessage(notificationData);
-        console.log("Booked! ------");
-      } else {
-        //underpayment --refund user and possibly email user of failed purchase attempt
-        data = contract.methods.processBid(logs[0].topic1, true).encodeABI();
-        // console.log("underpriced");
+
+        web3.eth
+          .sendTransaction(tx)
+          .on("transactionHash", (hash) => {
+            console.log(`Transaction hash: ${hash}`);
+          })
+          .on("receipt", (receipt) => {
+            console.log(
+              `Transaction was mined in block ${receipt.blockNumber}`
+            );
+            // res.send(200);
+          })
+          .on("error", (error) => {
+            console.error(error);
+            // res.send();
+            logger(JSON.stringify(err), "piProxy-listing", "error");
+          });
       }
-
-      //finish tx
-      const tx = {
-        from: account.address,
-        to: toAddress,
-        data,
-        gas: 100000,
-        gasPrice: web3.utils.toWei("50", "gwei"),
-      };
-
-      web3.eth
-        .sendTransaction(tx)
-        .on("transactionHash", (hash) => {
-          console.log(`Transaction hash: ${hash}`);
-        })
-        .on("receipt", (receipt) => {
-          console.log(`Transaction was mined in block ${receipt.blockNumber}`);
-          // res.send(200);
-        })
-        .on("error", (error) => {
-          console.error(error);
-          // res.send();
-          logger(JSON.stringify(err), "piProxy-listing", "error");
-        });
     }
   }
 
